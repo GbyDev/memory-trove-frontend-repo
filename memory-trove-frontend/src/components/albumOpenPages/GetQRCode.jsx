28e654860851fb15157dfc075b0c894c3da9b3df
GbyDev <kamimamita8kuji@gmail.com>
2025-05-11 09:21:13 +0800

    Implement GetQRCode component to display album name and QR code; include conversion of local path to web path for image rendering.

--- memory-trove-frontend/src/components/albumOpenPages/GetQRCode.jsx
+++ memory-trove-frontend/src/components/albumOpenPages/GetQRCode.jsx
@@ -1,3 +1,21 @@
+import { useContext } from "react";
+import { AlbumContext } from "../../contexts/AlbumContext";
+
+
 export default function GetQRCode(){
-    return(<h1>Get QR Code</h1>);
+    const {albumName,albumFolderPath} = useContext(AlbumContext);
+    const qrCodePath = albumFolderPath + "/qrcode/qrcode.png";
+    console.log(convertToWebPath(qrCodePath));
+    return(
+        <div className="main-container">
+            <h1>{albumName}</h1>
+            <p>QR Code</p>
+            <img src={convertToWebPath(qrCodePath)} alt="QR Code"></img>
+            <p>Scan it with your phone camera</p>
+        </div>
+    );
+}
+function convertToWebPath(localPath) {
+    //Replace local path with web path
+    return localPath.replace("C:/xampp/htdocs", "http://localhost");
 }
